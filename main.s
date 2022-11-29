d229833613967dd89becda286aa3e6c1116780b0
vs1720 <vs1720@skbl-422-012.ph.ic.ac.uk>
2022-11-29 11:45:27 +0000

    both servos moving

--- main.s
+++ main.s
@@ -1,6 +1,8 @@
+CONFIG  XINST = OFF            ; Extended Instruction Set (Disabled)
+
 #include <xc.inc>
 
-extrn servoStep, checkInterrupt, servoSetup
+extrn checkInterrupt, servoSetup
 psect code, abs
 rst:	org 0x0000
 	goto start
@@ -9,6 +11,7 @@ int_hi: org 0x0008
 	goto checkInterrupt
 
 start:
+    call servoSetup
     goto $
 	
 end rst
